9248c97ee8bf9f0f89cb43862a588d1fcd7b8904
Kevin Pilch-Bisson <kevin@pilch-bisson.net>
2015-08-08 17:49:09 -0700

    Add MvvmLight

---
+++ xunit.runner.wpf/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+using GalaSoft.MvvmLight;
+using System.Windows.Input;
+using System;
+using System.Windows;
+
+namespace xunit.runner.wpf.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
+        }
+    }
+}
