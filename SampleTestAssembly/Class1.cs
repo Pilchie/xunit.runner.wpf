09a2cdcda3c2eac5537568461ff14a076f86bc53
Kevin Pilch-Bisson <kevin@pilch-bisson.net>
2015-08-08 21:40:57 -0700

    Add some traits

--- SampleTestAssembly/Class1.cs
+++ SampleTestAssembly/Class1.cs
@@ -10,15 +10,18 @@ namespace SampleTestAssembly
     public class Class1
     {
         [Fact]
+        [Trait("TraitName1", "TraitValue1")]
         public void Pass() { }
 
         [Fact]
+        [Trait("TraitName1", "TraitValue2")]
         public void Fail()
         {
             Assert.True(false);
         }
 
         [Fact(Skip = "Testing")]
+        [Trait("TraitName2", "TraitValue2")]
         public void Skip() { }
     }
 }
