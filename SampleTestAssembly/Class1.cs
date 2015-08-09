9387c635d4ab940f7a746e7e4af76aa36e00363f
Kevin Pilch-Bisson <kevin@pilch-bisson.net>
2015-08-08 21:00:48 -0700

    Add a project with some tests

---
+++ SampleTestAssembly/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SampleTestAssembly
+{
+    public class Class1
+    {
+        [Fact]
+        public void Pass() { }
+
+        [Fact]
+        public void Fail()
+        {
+            Assert.True(false);
+        }
+
+        [Fact(Skip = "Testing")]
+        public void Skip() { }
+    }
+}
