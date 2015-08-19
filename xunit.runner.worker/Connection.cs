0f1ce7705db6ab406148f4a0627383a684013be5
Jared Parsons <jaredpar@microsoft.com>
2015-08-19 14:05:24 -0700

    Work

---
+++ xunit.runner.worker/Connection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xunit.runner.worker
+{
+    internal abstract class Connection : IDisposable
+    {
+        private readonly Stream _stream;
+        private bool _closed;
+
+        internal Stream Stream => _stream;
+
+        protected abstract void WaitForClientConnect();
+        protected abstract void WaitForClientDone();
+
+        protected virtual void DisposeCore()
+        {
+
+        }
+
+        private void Dispose()
+        {
+            if (_closed)
+            {
+                return;
+            }
+
+            _closed = true;
+            DisposeCore();
+        }
+
+        #region IDisposable
+
+        void IDisposable.Dispose()
+        {
+            Dispose();
+        }
+
+        #endregion
+    }
+
+    internal sealed class NamedPipeConnection : Connection
+    {
+        internal NamedPipeConnection
+
+    }
+}
