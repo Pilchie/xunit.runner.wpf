e41071ddb03322da82c27c21015306e66273ff6f
Fredric Silberberg <frsilb@microsoft.com>
2017-06-02 16:19:08 -0700

    Added autoreloading of test assemblies

---
+++ xunit.runner.wpf/ITestAssemblyWatcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Xunit.Runner.Wpf
+{
+    internal interface ITestAssemblyWatcher
+    {
+        /// <summary>
+        /// Adds a new assembly to list of assemblies to be autoreloaded.
+        /// </summary>
+        void AddAssembly(string assemblyFileName);
+
+        /// <summary>
+        /// Removes an assembly from the list of assemblies ot be autoreloaded.
+        /// </summary>
+        void RemoveAssembly(string assemblyFileName);
+
+        /// <summary>
+        /// Enables watching of all assemblies.
+        /// </summary>
+        /// <param name="reloader">Action to perform when a file change is detected</param>
+        void EnableWatch(Func<IEnumerable<string>, bool> reloader);
+
+        /// <summary>
+        /// Disables watching of all assemblies
+        /// </summary>
+        void DisableWatch();
+    }
+}
